0bbd732e48e7abbd637a15a523dd202e1830592f
alopezorozco <alopezorozco@uaz.edu.mx>
2023-05-04 08:01:38 -0600

    Commit 3 - Se completo el código de esta clase para poder guardar la palabra adivinada

--- src/main/java/com/wordscramble/words/models/AciertoDaoImp.java
+++ src/main/java/com/wordscramble/words/models/AciertoDaoImp.java
@@ -1,8 +1,19 @@
 package com.wordscramble.words.models;
 
 import com.wordscramble.words.dao.AciertoDao;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+@Transactional
 public class AciertoDaoImp implements AciertoDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
     @Override
     public void guardarAcierto(int idUsuario, int idPalabra) {
         //creamos un objeto de tipo AciertoId
@@ -11,6 +22,29 @@ public class AciertoDaoImp implements AciertoDao {
         //creamos un objeto de tipo Acierto
         Acierto miAcierto = new Acierto();
 
+            //obtenemos los objetos Usuario y Palabra con base a su Id.
+            miAcierto.setPalabra(obtenerPalabraPorId(idPalabra));
+            miAcierto.setUsuario(obtenerUsuarioPorId(idUsuario));
 
+        //guardamos el nuevo registro
+        entityManager.merge(miAcierto);
+    }
+
+    /**
+     * Obtiene la palabra según el id especificado
+     * @param idPalabra
+     * @return
+     */
+    private Palabra obtenerPalabraPorId(int idPalabra) {
+        return entityManager.find(Palabra.class, idPalabra);
+    }
+
+    /**
+     * Obtiene el usuario según el id. del usuario
+     * @param idUsuario
+     * @return
+     */
+    private Usuario obtenerUsuarioPorId(int idUsuario){
+        return entityManager.find(Usuario.class, idUsuario);
     }
 }
