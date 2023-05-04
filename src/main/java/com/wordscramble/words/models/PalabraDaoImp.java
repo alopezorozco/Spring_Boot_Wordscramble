538f720b84231c04e1c885d569a22440fb2aeff0
alopezorozco <alopezorozco@uaz.edu.mx>
2023-05-03 22:20:26 -0600

    Commit 3 - Creación de la clase PalabraDaoImp

--- src/main/java/com/wordscramble/words/models/PalabraDaoImp.java
+++ src/main/java/com/wordscramble/words/models/PalabraDaoImp.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -15,8 +16,30 @@ public class PalabraDaoImp implements PalabraDao {
     @PersistenceContext
     EntityManager entityManager;
 
+    /**
+     * Obtiene una lista de palabras no acertadas según la asignatura
+     * @param idAsignatura
+     * @return
+     */
     @Override
     public List<Palabra> obtenerPalabraAAdivinar(int idAsignatura) {
-        return null;
-    }
-}
+        //consulta HQL para obtener todas las palabras que no han sido adivinadas
+        //por el usuario y que pertenezcan a la asignatura pasada como parámetro
+        String hql = "SELECT p " +
+                "FROM acierto a RIGHT JOIN a.palabra p " +
+                "WHERE p.asignagura.id = : asignaturaId AND a.aciertoId.palId IS NULL";
+
+        //ejetucamos la consulta y le pasamos el parámetro correspondiente
+        Query query = entityManager.createQuery(hql);
+        query.setParameter("asignaturaId", idAsignatura);
+
+        //especificamos que la consulta sólo retorne un resultado
+        query.setMaxResults(1);
+
+        //asignamos el resultado a la lista resutl
+        List<Palabra> result = query.getResultList();
+
+        //retornamos el resultado
+        return result;
+    }//fin del método obtenerPalabraAAdivinar
+}//fin de la clase
